3a56b2e12bd00cac033cbf1dc94d019d35909822
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-05-31 21:43:07 -0700

    Add logic for CRUD

--- bamazon.sql
+++ bamazon.sql
@@ -12,16 +12,16 @@ CREATE TABLE products (
   stock_quantity INTEGER(10)
 );
 
-INSERT INTO products(item_id,product_name,department_name,price,stock_quantity) VALUES(1,"HeadPhones","Accessories",100,500);
-INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("SanDisk Pendrive","Electronics",100,500);
-INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("Fitbit","Wearable Technology",100,500);
-INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("Fossil","Watch",100,500);
-INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("Samsung Smart Tv","Electronics",100,500);
-INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("Philips Trimmer","Accessories",100,500);
-INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("Necklaces","Jewelery",100,500);
-INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("One Plus 6","Cell Phones",100,500);
-INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("Running Shoes","Sports Accessories",100,500);
-INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("Gift Cards","Gifts",100,500);
+INSERT INTO products(item_id,product_name,department_name,price,stock_quantity) VALUES(1,"HeadPhones","Accessories",100,200);
+INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("SanDisk Pendrive","Electronics",20,500);
+INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("Fitbit","Wearable Technology",200,500);
+INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("Fossil","Watch",250,100);
+INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("Samsung Smart Tv","Electronics",900,100);
+INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("Philips Trimmer","Accessories",50,500);
+INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("Necklaces","Jewelery",30,300);
+INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("One Plus 6","Cell Phones",600,100);
+INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("Running Shoes","Sports Accessories",60,1000);
+INSERT INTO products(product_name,department_name,price,stock_quantity) VALUES("Gift Cards","Gifts",10,400);
 
 
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,33 +1,122 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("console.table");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "password",
-    database: "bamazon"
-  });
-  
-  connection.connect(function(err) {
-    if (err) throw err;
-    showItems();
-  });
+  host: "localhost",
 
-  function showItems() {
-    connection.query("SELECT * FROM products", function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-            
-          console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + "|" + res[i].stock_quantity);
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  showItems();
+});
+
+function showItems() {
+  connection.query("SELECT * FROM products", function(error, response) {
+    if (error) throw error;
+    console.table(response);
+    console.log("------------------------------------------------------");
+    askUser(response);
+  });
+}
+
+
+
+function askUser(products) {
+  inquirer
+    .prompt([
+      {
+        // The first should ask them the ID of the product they would like to buy.
+        type: "input",
+        name: "id",
+        message: "What is the ID of the item you would like to purchase?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
         }
-        console.log("-----------------------------------");
-      });
-    }    
-  
-  
+
+      },
+      {
+        // The second message should ask how many units of the product they would like to buy.
+        type: "input",
+        name: "quantity",
+        message: "How many would you like to purchase?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+    .then(function(answer) {
+      //log the response obtained from the prompt
+      console.log("Answer", answer);
+      console.log("Products", products);
+      //declare variables to store the user inputs for id and quantity
+      var itemChosen = parseInt(answer.id);
+      var quantityChosen = answer.quantity;
+
+      var productChosen = products[itemChosen-1];
+      
+      if (!productChosen) {
+        console.log("Invalid ID, Please enter a correct ID from the table");
+        showItems();
+      }
+      
+      var price = productChosen.price;
+      //if the stock_quantity is lesser than the quantity entered by the user, consider it as successful purchase
+      if (quantityChosen < productChosen.stock_quantity) {
+        //log the success message for the user and show the final price
+        console.log(
+          "Product added to your cart! You pay: " +
+            "$" +
+            quantityChosen * price
+        );
+        updateStocks(productChosen, quantityChosen);
+      } else {
+        //else ask the user to  Enter lesser quantity
+        console.log("Oops! Out of Stock! Try Again");
+        //show the table of products again for the user to make a choice again
+        showItems();
+      }
+    });
+}
+
+function updateStocks(productChosen, quantityChosen) {
+  //log a message for the user that the stocks are up to date
+  console.log("Stocks are up to date");
+  //make an UPDATE query to the DB where stocks = original stock - user chosen quantity
+  connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      {
+        stock_quantity: productChosen.stock_quantity - quantityChosen
+      },
+      {
+        item_id: productChosen.item_id
+      }
+    ],
+    function(err, res) {
+      if (err) throw err;
+      //log the rows affected after UPDATE is performed
+      console.log(res.affectedRows);
+      //show the updated table again
+      showItems();
+      //end connection to DB
+      connection.end();
+    }
+  );
+}
